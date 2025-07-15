1cbc848226481014e4a2ae3d47345937aee7da09
ItsAchance <alexander.chance@tutanota.com>
2025-07-15 16:34:36 +0200

    Added check for gitops.log file, appending date output to log and absolut path to files

--- gitops-sync.sh
+++ gitops-sync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-REPO_DIR="~/code/homelab"
+REPO_DIR="/Users/achance/code/homelab"
 REPO_URL="https://github.com/ItsAchance/homelab.git"
 BRANCH="main"
-LOG_FILE="~/code/homelab/gitops.log"
+LOG_FILE="${REPO_DIR}/gitops.log"
 DOCKER_COMPOSE_FILE="docker-compose.yaml"
 
 cd "$REPO_DIR" || git clone --branch "$BRANCH" "$REPO_URL" "$REPO_DIR" && cd "$REPO_DIR"
@@ -13,19 +13,24 @@ git fetch origin "$BRANCH"
 LOCAL_HASH=$(git rev-parse HEAD)
 REMOTE_HASH=$(git rev-parse origin/"$BRANCH")
 
+if [ ! -f ${LOG_FILE} ]; then
+    echo "$(date)[INFO] ${LOG_FILE} not present, creating it..." >> ${LOG_FILE}
+    touch ${LOG_FILE}
+fi
+
 if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
-  echo "[INFO] New commit found, pulling changes..." > ${LOG_FILE}
+    echo "$(date)[INFO] New commit found, pulling changes..." >> ${LOG_FILE}
   git pull origin "$BRANCH"
 
-  echo "[INFO] Validating ${DOCKER_COMPOSE_FILE}..." > ${LOG_FILE}
+  echo "[INFO] Validating ${DOCKER_COMPOSE_FILE}..." >> ${LOG_FILE}
   if docker-compose -f ${DOCKER_COMPOSE_FILE} config >/dev/null 2>&1; then
-    echo "[INFO] Compose file is valid. Redeploying..." > ${LOG_FILE}
+      echo "$(date)[INFO] Compose file is valid. Redeploying..." >> ${LOG_FILE}
     docker-compose -f ${DOCKER_COMPOSE_FILE} pull
     docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
   else
-    echo "[ERROR] Invalid docker-compose.yml. Skipping deploy." > ${LOG_FILE}
+      echo "$(date)[ERROR] Invalid docker-compose.yml. Skipping deploy." >> ${LOG_FILE}
   fi
 else
-  echo "[INFO] No changes detected. Skipping." > ${LOG_FILE}
+    echo "$(date)[INFO] No changes detected. Skipping." >> ${LOG_FILE}
 fi
 
