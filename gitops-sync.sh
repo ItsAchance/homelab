184f8698cdfaaf0a1c1d743703a4a962385663eb
ItsAchance <alexander.chance@tutanota.com>
2025-07-15 16:41:24 +0200

    Fixed formatting of log messages to be more readable

--- gitops-sync.sh
+++ gitops-sync.sh
@@ -14,23 +14,23 @@ LOCAL_HASH=$(git rev-parse HEAD)
 REMOTE_HASH=$(git rev-parse origin/"$BRANCH")
 
 if [ ! -f ${LOG_FILE} ]; then
-    echo "$(date)[INFO] ${LOG_FILE} not present, creating it..." >> ${LOG_FILE}
+    echo "[INFO] $(date) ${LOG_FILE} not present, creating it..." >> ${LOG_FILE}
     touch ${LOG_FILE}
 fi
 
 if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
-    echo "$(date)[INFO] New commit found, pulling changes..." >> ${LOG_FILE}
+    echo "[INFO] $(date) New commit found, pulling changes..." >> ${LOG_FILE}
   git pull origin "$BRANCH"
 
-  echo "[INFO] Validating ${DOCKER_COMPOSE_FILE}..." >> ${LOG_FILE}
+  echo "[INFO] $(date) Validating ${DOCKER_COMPOSE_FILE}..." >> ${LOG_FILE}
   if docker-compose -f ${DOCKER_COMPOSE_FILE} config >/dev/null 2>&1; then
-      echo "$(date)[INFO] Compose file is valid. Redeploying..." >> ${LOG_FILE}
+      echo "[INFO] $(date) Compose file is valid. Redeploying..." >> ${LOG_FILE}
     docker-compose -f ${DOCKER_COMPOSE_FILE} pull
     docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
   else
-      echo "$(date)[ERROR] Invalid docker-compose.yml. Skipping deploy." >> ${LOG_FILE}
+      echo "[ERROR] $(date) Invalid compose file. Skipping deploy." >> ${LOG_FILE}
   fi
 else
-    echo "$(date)[INFO] No changes detected. Skipping." >> ${LOG_FILE}
+    echo "[ERROR] $(date) No changes detected. Skipping." >> ${LOG_FILE}
 fi
 
