97ac4efc86d28d13d2eaae287e0f758e03bbe6a2
ItsAchance <alexander.chance@tutanota.com>
2025-07-16 14:27:17 +0200

    Fixed inconsitencies for calling vars and git fetch was missing

--- gitops-sync.sh
+++ gitops-sync.sh
@@ -11,28 +11,22 @@ BRANCH="main"
 LOG_FILE="${REPO_DIR}/gitops.log"
 DOCKER_COMPOSE_FILE="docker-compose.yaml"
 
-echo "[INFO] Checking for Git repo in ${REPO_DIR}" >> ${LOG_FILE}
-if [ ! -d "${REPO_DIR}/.git" ]; then
-    echo "[INFO] Git repo not found in ${REPO_DIR}. Cloning..." >> ${LOG_FILE}
-    rm -rf "$REPO_DIR"
-    git clone --branch "$BRANCH" "$REPO_URL" "$REPO_DIR"
-fi
-    
-cd $REPO_DIR
+cd ${REPO_DIR}
+git fetch origin "${BRANCH}"
 
 LOCAL_HASH=$(git rev-parse HEAD)
-REMOTE_HASH=$(git rev-parse origin/"$BRANCH")
+REMOTE_HASH=$(git rev-parse origin/"${BRANCH}")
 
 if [ ! -f ${LOG_FILE} ]; then
     echo "[INFO] $(date) ${LOG_FILE} not present, creating it..." >> ${LOG_FILE}
     touch ${LOG_FILE}
 fi
 
-if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+if [ "${LOCAL_HASH}" != "${REMOTE_HASH}" ]; then
     echo "[INFO] $(date) New commit found, pulling changes..." >> ${LOG_FILE}
-    git fetch origin "$BRANCH"
-    git reset --hard origin/"$BRANCH"
-    git pull origin "$BRANCH"
+    git fetch origin "${BRANCH}"
+    git reset --hard origin/"${BRANCH}"
+    git pull origin "${BRANCH}"
 
     echo "[INFO] $(date) Validating ${DOCKER_COMPOSE_FILE}..." >> ${LOG_FILE}
     if docker compose -f ${DOCKER_COMPOSE_FILE} config >/dev/null 2>&1; then
